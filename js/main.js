6be5e9d66ae7ebd454ec5d08b4f33d14ad520e20
Rietho626 <rietho626@gmail.com>
2026-06-05 17:28:54 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch('https://rietho626/pythonanywhere.com').then(res=>res.json()).then(data=>console.log(data.msg));
+fetch('https://rietho626.pythonanywhere.com').then(res=>res.json()).then(data=>console.log(data.msg));
 
 const container = document.getElementById('test');
 const stringo = "Geäst und trockenes Laub knirschen zu deinen Füßen. Deine Schritte, schwerfällig, ziellos. Tiefer und tiefer ins nächtliche Halbdunkel des Waldes, führen sie dich. Vorbei an tagsüber majestätischen Eichen, gespenstisch im fahlen Licht des Mondes. Ohne zu wissen weshalb, wandelst du voran. Es scheint nicht von Bedeutung zu sein.";
