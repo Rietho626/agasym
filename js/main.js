f49e827583e6f906757316cf06d68d65ea09f60a
Rietho626 <rietho626@gmail.com>
2026-06-05 21:29:05 +0200

    some updates

--- js/main.js
+++ js/main.js
@@ -2,17 +2,25 @@
 async function getByCode(code){
     return await fetch('https://rietho626.pythonanywhere.com/0').then(res=>res.json()).then(data=>data);
     
+
 }
+const container = document.getElementById('test');
 
 async function startGame() {
     const snippet = await getByCode(0);
-    storyTime(snippet);
+    storyTime(snippet, "");
 }
 
 startGame();
 
-const container = document.getElementById('test');
-async function storyTime(snippet){
+async function continueGame(code){
+    Array.from(document.querySelectorAll('.choice-box')).forEach(node=>container.removeChild(node));
+    const snippet = await getByCode(code);
+    storyTime(snippet, code);
+}
+
+
+async function storyTime(snippet, code){
     const story = snippet.text;
     let told = "";
     for(let i = 0; i < story.length; i++){      
@@ -26,11 +34,11 @@ async function storyTime(snippet){
         }
     }
     snippet.choices.forEach(choice=>{
-        appendChoices(container, choice);
+        appendChoices(container, choice, code);
     })
 }
 
-function appendChoices(parent, choice){
+function appendChoices(parent, choice, code){
     if(!choice.code.includes('H')){
         const choiceBox = document.createElement('div');
         choiceBox.classList.add('choice-box');
@@ -41,11 +49,20 @@ function appendChoices(parent, choice){
         choiceMain.textContent = choice.main;
         choiceSub.textContent = choice.sub;
 
+        choiceBox.addEventListener("click", ()=>{
+            continueGame(fullCode, choice.code);
+        })
+
         choiceBox.appendChild(choiceMain);
         choiceBox.appendChild(choiceSub);
         parent.appendChild(choiceBox);
     }
 }
+
+function choiceBoxListener(fullCode, choiceCode){
+    continueGame(String(fullCode)+String(choiceCode));
+}
+
 
 function delay(time){
     return new Promise((resolve)=>{
