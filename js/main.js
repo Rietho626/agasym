92114eb1cdf5cbf6b03ebfd3cdcaed6ef22ff8ae
Rietho626 <rietho626@gmail.com>
2026-06-05 20:45:49 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-fetch('https://rietho626.pythonanywhere.com').then(res=>res.json()).then(data=>console.log(data.msg));
 
+async function getByCode(code){
+    const snippet = await fetch('https://rietho626.pythonanywhere.com/0').then(res=>res.json()).then(data=>data);
+    console.log(snippet);
+}
+
+getByCode(0);
 const container = document.getElementById('test');
-const stringo = "Geäst und trockenes Laub knirschen zu deinen Füßen. Deine Schritte, schwerfällig, ziellos. Tiefer und tiefer ins nächtliche Halbdunkel des Waldes, führen sie dich. Vorbei an tagsüber majestätischen Eichen, gespenstisch im fahlen Licht des Mondes. Ohne zu wissen weshalb, wandelst du voran. Es scheint nicht von Bedeutung zu sein.";
 async function storyTime(story){
     let told = "";
     for(let i = 0; i < story.length; i++){      
@@ -15,8 +19,6 @@ async function storyTime(story){
         }
     }
 }
-
-storyTime(stringo);
 
 function delay(time){
     return new Promise((resolve)=>{
