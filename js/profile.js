eece0c47d7e7dc9c42e31fd0b270c84487271d23
Rietho626 <rietho626@gmail.com>
2026-06-11 16:34:34 +0200

    profile page added

--- js/profile.js
+++ js/profile.js
@@ -9,4 +9,38 @@ const characters = await fetch(`https://rietho626.pythonanywhere.com/api/login-c
             }
         ).then(res=>res.json());
 
-console.log(characters);
+
+const wrapper = document.getElementById("wrapper");
+
+if(!characters.response){
+    wrapper.innerHTML = "You are not logged in, you fool.";
+}else{
+    const uname = localStorage.getItem("uname");
+    const greeting = document.getElementById("greeting");
+    const characterBoxes = document.getElementById("characters");
+    const createNew = document.getElementById("create-new-character");
+    const deleteChar = document.getElementById("delete-character");
+    const charInfo = document.getElementById("char-info");
+    const logout = document.getElementById("logout-button");
+    const erfolge = document.getElementById("erfolge-button");
+
+    greeting.textContent = `Seien Sie gegrüßt, ${uname}`;
+
+    if(characters.chars.length){
+        characters.chars.forEach(char=>{
+            const charBox = document.createElement("div");
+            charBox.classList.add("char-box");
+            const charName = document.createElement("div");
+            charName.textContent = char["char_name"];
+            const charScene = document.createElement("div");
+            charScene.textContent = char["story_code"];
+            charBox.appendChild(charName);
+            charBox.appendChild(charScene);
+            characterBoxes.appendChild(charBox);
+        })
+    }else{
+        characters.textContent = 'Wähle "beginne eine neue Reise", um aufzubrechen';
+    }
+
+
+}
